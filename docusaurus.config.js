de5cc188998d1c1862bff13fdcb6ceb3d746dd67
Tiago Felipe <tiago@tiagofelipe.com>
2024-04-10 02:21:55 -0300

    Dev

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,8 +30,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR', 'en'],
   },
 
   presets: [
